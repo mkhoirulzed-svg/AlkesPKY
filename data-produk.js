f361fd7138753095db68871177b9bf7d1e805edb
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-18 21:16:55 +0800

    Create data-produk.js

---
+++ data-produk.js
@@ -0,0 +1,19 @@
+<script>
+const produkShopee = [
+  {
+    nama: "Tensimeter Digital",
+    harga: "Rp 350.000",
+    gambar: "https://cf.shopee.co.id/file/xxxx.jpg"
+  },
+  {
+    nama: "Nebulizer Portable",
+    harga: "Rp 420.000",
+    gambar: "https://cf.shopee.co.id/file/yyyy.jpg"
+  },
+  {
+    nama: "Stetoskop Dewasa",
+    harga: "Rp 95.000",
+    gambar: "https://cf.shopee.co.id/file/zzzz.jpg"
+  }
+];
+</script>
