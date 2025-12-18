2933ac3818f556940c87fa9d56a328983d43836b
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-18 21:35:34 +0800

    Update data-produk.js

--- data-produk.js
+++ data-produk.js
@@ -1,8 +1,8 @@
 const produkShopee = [
   {
-    nama: "Tensimeter Digital",
-    harga: "Rp 350.000",
-    gambar: "https://cf.shopee.co.id/file/xxxx.jpg"
+    nama: "Tensimeter Digital Akurat",
+    harga: "Rp 70.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134207-8224r-mi1edow0vyf74c@resize_w900_nl.webp"
   },
   {
     nama: "Nebulizer Portable",
