5e9ef8f486d2921c9a5ff900a93d14ffe5091eda
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-18 23:17:45 +0800

    Update data-produk.js

--- data-produk.js
+++ data-produk.js
@@ -5,6 +5,11 @@ const produkShopee = [
     gambar: "https://down-id.img.susercontent.com/file/id-11134207-8224r-mi1edow0vyf74c@resize_w900_nl.webp"
   },
   {
+     nama: "Easytouch 3 in 1",
+    harga: "Rp 250.000",
+    gambar: "https://cf.shopee.co.id/file/id-11134207-8224u-mi2meeu04s938f_tn"
+  },
+  {
      nama: "Tensimeter Digital iTrust FC BP121",
     harga: "Rp 140.000",
     gambar: "https://down-id.img.susercontent.com/file/id-11134207-82252-mib515ngufwj77@resize_w900_nl.webp"
@@ -18,5 +23,20 @@ const produkShopee = [
     nama: "Nebulizer portable",
     harga: "Rp 65.000",
     gambar: "https://upload.jaknot.com/2025/09/images/products/9f0fe9/original/taffomicron-alat-terapi-pernapasan-mesh-nebulizer-portable-inhaler-jsl-w302.jpg"
+  },
+   {
+    nama: "Tensimeter Taff omicron BW750",
+    harga: "Rp 130.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134275-7ra0g-mct0thw4rozb21@resize_w900_nl.webp"
+  },
+  {
+    nama: "Termometer digital-Termoone",
+    harga: "Rp 20.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134207-7rbke-m7u6s7hfbl5l86@resize_w900_nl.webp"
+  },
+   {
+    nama: "Termogun",
+    harga: "Rp 55.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134207-7ras8-m62e7pp4jwg603@resize_w900_nl.webp"
   },
 ];
