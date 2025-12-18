03c32f22186a14acae846e58a2f8a767de503c3b
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-18 22:50:45 +0800

    Update data-produk.js

--- data-produk.js
+++ data-produk.js
@@ -5,13 +5,18 @@ const produkShopee = [
     gambar: "https://down-id.img.susercontent.com/file/id-11134207-8224r-mi1edow0vyf74c@resize_w900_nl.webp"
   },
   {
-    nama: "Nebulizer Portable",
-    harga: "Rp 420.000",
-    gambar: "https://cf.shopee.co.id/file/yyyy.jpg"
+     nama: "Tensimeter Digital iTrust FC BP121",
+    harga: "Rp 140.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134207-82252-mib515ngufwj77@resize_w900_nl.webp"
   },
   {
-    nama: "Stetoskop Dewasa",
-    harga: "Rp 95.000",
-    gambar: "https://cf.shopee.co.id/file/zzzz.jpg"
-  }
+    nama: "Tensimeter Digital Omron HEM-7124",
+    harga: "Rp 360.000",
+    gambar: "https://down-id.img.susercontent.com/file/id-11134207-7r98y-ltq8vyk8y4gz03@resize_w900_nl.webp"
+  },
+  {
+    nama: "Nebulizer portable",
+    harga: "Rp 65.000",
+    gambar: "https://upload.jaknot.com/2025/09/images/products/9f0fe9/original/taffomicron-alat-terapi-pernapasan-mesh-nebulizer-portable-inhaler-jsl-w302.jpg"
+  },
 ];
